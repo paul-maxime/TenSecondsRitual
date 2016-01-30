d35a267b74677f10139699f470513a56c9581285
Paul-Maxime <paul-maxime@users.noreply.github.com>
2016-01-30 01:14:27 +0000

    Add the game timer

--- scripts/game.js
+++ scripts/game.js
@@ -2,6 +2,50 @@
  * Ten Seconds Ritual
  */
 
+Game = {
+    timer: 0,
+    now: 0,
+};
+
+Game.initialize = function () {
+    Game.startLevel();
+};
+
+Game.startLevel = function () {
+    Game.setTimer(12000);
+    Game.now = Date.now();
+    setInterval(function () {
+	var now = Date.now();
+	var delta = now - Game.now;
+	Game.now = now;
+	Game.setTimer(Game.timer - delta);
+    }, 70);
+};
+
+Game.setTimer = function (value) {
+    Game.timer = value;
+    var seconds = Math.floor(value / 1000) + "";
+    var milliseconds = Math.floor((value / 10) % 100) + "";
+    if (seconds.length < 2) {
+	seconds = "0" + seconds;
+    }
+    if (milliseconds.length < 2) {
+	milliseconds = "0" + milliseconds;
+    }
+    var text = seconds + ":" + milliseconds;
+    $('#game-timer').text(text);
+    if (Game.timer < 5000) {
+	$('#game-timer').addClass('game-timer-warning');
+    } else {
+	$('#game-timer').removeClass('game-timer-warning');
+    }
+    if (Game.timer < 2000) {
+	$('#game-timer').addClass('game-timer-alert');
+    } else {
+	$('#game-timer').removeClass('game-timer-alert');
+    }
+};
+
 $(function () {
-    console.log('hello, world');
+    Game.initialize();
 });
